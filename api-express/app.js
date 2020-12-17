2e79a56183fee222f58932531ca19c56a4d53fd9
unknown <alpenprastoyo@gmail.com>
2020-12-17 22:37:57 +0700

    upload service media

--- api-express/app.js
+++ api-express/app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index');  
 const usersRouter = require('./routes/users');
 const coursesRouter = require('./routes/courses');
 const mediaRouter = require('./routes/media');
@@ -15,8 +15,8 @@ const paymentsRouter = require('./routes/payments');
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- api-express/routes/apiAdapter.js
+++ api-express/routes/apiAdapter.js
@@ -3,8 +3,8 @@ const axios = require('axios');
 const { TIMEOUT } = process.env;
 
 module.exports = (baseUrl) => {
-    return axios.crete({
-        baseUrl: baseUrl,
-        timeout: TIMEOUT
-    })
+  return axios.create({
+    baseURL: baseUrl,
+    timeout: parseInt(TIMEOUT)
+  });
 }
