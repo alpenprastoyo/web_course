8d654bd70f1c090095ea9dba7cdd2e804e82c0eb
AlpenPrastoyo <alpenprastoyo@gmail.com>
2021-01-04 05:51:56 +0700

    add service course

--- api-express/routes/handler/users/index.js
+++ api-express/routes/handler/users/index.js
@@ -1,8 +1,17 @@
 const register = require('./register');
 const login = require('./login');
+const update = require('./update');
+const getUser = require('./getUser');
+const logout = require('./logout');
+
+
 
 
 module.exports = {
+    update,
     register,
-    login
+    login,
+    getUser,
+    logout
+    
 }
